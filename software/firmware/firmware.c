c591c032e4ec4b3b004a06734e7d027bdf7d8d34
ricardo-lorena <ricardolorena@tecnico.ulisboa.pt>
2020-11-17 22:15:14 +0000

    2pergunta

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -2,15 +2,23 @@
 #include "system.h"
 #include "periphs.h"
 #include "iob-uart.h"
-
+#include "time.h"
+//antigo
 int main()
 {
-  //init uart 
-   int i, t1=0, t2=1, nextTerm;
- uart_init(UART_BASE,FREQ/BAUD);
-  for (i=1;i<=100;++i){
-    uart_printf("%d\n",t1);
-	nextTerm=t1+t2;
-	t1=t2;
-	t2=nextTerm;}
+        int y=0;
+	int y_1=0;
+	int i,n;
+	int x ;
+	time_t t;
+	uart_init(UART_BASE,FREQ/BAUD);
+	srand((unsigned) time(&t));
+	
+	for(i=0;i<100; i++){
+x=(rand());
+y=2*y_1+x;
+uart_printf("%d\n",y);
+    
+
+y_1=y;}
 }
